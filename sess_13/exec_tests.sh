2e36384c8c95f591e135ff8d4b8b3fe60b356ab5
Ender <nallahabi2003@gmail.com>
2025-04-26 21:49:06 -0400

    removed extra folders/files

--- sess_13/exec_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo '------ Starting Tests -----'
-python3 tokenizer.py && \
-python3 parser.py && \
-python3 evaluator.py && \
-python3 runner.py && \
-echo '---- All Tests Passed! ----'
--- sess_8/runme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ $# == 1 ]; then
-    /usr/bin/python3 runner.py $1
-else
-    echo "Usage $0 [file]"
-fi
